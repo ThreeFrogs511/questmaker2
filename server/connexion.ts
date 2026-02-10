5ffe9c1184317f8936f893114fe964cfd0903726
lavarde <nicolas.lavarde@gmail.com>
2026-02-10 10:20:04 +0100

    Fix Neon SSL + use DATABASE_URL

--- server/connexion.ts
+++ server/connexion.ts
@@ -7,7 +7,8 @@ export const sql = postgres({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT)
+  port: Number(process.env.DB_PORT),
+  ssl: "require",
 });
 
 
